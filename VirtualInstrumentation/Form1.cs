2044225e65debbeeeb1ae2757b334c664f042adc
Егор Четвертков <egor1er8@mail.ru>
2022-02-17 14:48:55 +0700

    add instrumentalMessage

--- VirtualInstrumentation/Form1.cs
+++ VirtualInstrumentation/Form1.cs
@@ -5,6 +5,17 @@ namespace VirtualInstrumentation
 {
     public partial class MainForm : Form
     {
+        public event Action Start;
+        public event Action Stop;
+        public event Action Exit;
+
+        public event Action<double> W0Change;
+        public event Action<double> W1Change;
+        public event Action<double> W2Change;
+        public event Action<double> W3Change;
+        public event Action<double> W4Change;
+        public event Action<double> W5Change;
+
         public MainForm()
         {
             InitializeComponent();
@@ -12,45 +23,75 @@ namespace VirtualInstrumentation
 
         private void StartBtn_Click(object sender, EventArgs e)
         {
-
+            Start?.Invoke();
         }
 
         private void StopBtn_Click(object sender, EventArgs e)
         {
-
+            Stop?.Invoke();
         }
 
         private void ExitBtn_Click(object sender, EventArgs e)
         {
-
+            Exit?.Invoke();
         }
 
         private void Weight0NUD_ValueChanged(object sender, EventArgs e)
         {
-
+            W0Change?.Invoke(Convert.ToDouble(Weight0NUD.Value));
         }
 
         private void Weight3NUD_ValueChanged(object sender, EventArgs e)
         {
-
+            W3Change?.Invoke(Convert.ToDouble(Weight3NUD.Value));
         }
 
         private void Weight1NUD_ValueChanged(object sender, EventArgs e)
         {
-
+            W1Change?.Invoke(Convert.ToDouble(Weight1NUD.Value));
         }
 
         private void Weight4NUD_ValueChanged(object sender, EventArgs e)
         {
-
+            W4Change?.Invoke(Convert.ToDouble(Weight4NUD.Value));
         }
 
         private void Weight2NUD_ValueChanged(object sender, EventArgs e)
         {
-
+            W2Change?.Invoke(Convert.ToDouble(Weight2NUD.Value));
         }
 
         private void Weight5NUD_ValueChanged(object sender, EventArgs e)
+        {
+            W5Change?.Invoke(Convert.ToDouble(Weight5NUD.Value));
+        }
+
+        public void UpdateChart(double newY, double newX)
+        {
+            //TODO. Add new point to Series. Shift Chart Area.
+        }
+
+        public void UpdateBaseXs(double x0, double x1, double x2)
+        {
+            //TODO. Print value xs in TBs
+        }
+
+        public void UpdateFilterXs(double x0, double x1, double x2)
+        {
+            //TODO. Print value xs in TBs
+        }
+
+        public void UpdateBaseY(double y)
+        {
+            //TODO. Print value y in TB
+        }
+
+        public void UpdateFilterY(double y)
+        {
+            //TODO. Print value y in TB
+        }
+
+        public void PrintMessage(InstrumentMessage message)
         {
 
         }
---
+++ VirtualInstrumentation/InstrumentMessage.cs
@@ -0,0 +1,31 @@
+﻿namespace VirtualInstrumentation
+{
+    public class InstrumentMessage
+    {
+        public string Message { get; private set; }
+        public int Code { get; private set; }
+        public string Type { get; private set; }
+
+        public InstrumentMessage(TypeMessage type, string message)
+        {
+            Code = (int)type;
+            Type = type.ToString();
+            Message = message;
+        }
+
+        public override string ToString()
+        {
+            return $"{Type}\n{Code}\n{Message}";
+        }
+
+        public override int GetHashCode()
+        {
+            return Code;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj != null && obj is InstrumentMessage other && other.Code == Code;
+        }
+    }
+}
